b28465b4f7dd0dd30cf9a0ae21ab6daa8ee27ea9
niezwyklyable <niezwyklyable@gmail.com>
2021-11-07 18:49:15 +0100

    Decreasing the size of a car
    	modified:   cc/constants.py

--- cc/constants.py
+++ cc/constants.py
@@ -8,13 +8,16 @@ FF_POS_X, FF_POS_Y = 175, 270
 # finish flag target dims
 FF_TARGET_W, FF_TARGET_H = 70, 20
 
+# car size reductor
+COEF = 0.7
+
 # assets
 BACKGROUND = scale(load('imgs/grass.jpg'), (WIDTH, HEIGHT))
 TRACK = scale(load('imgs/track.png'), (WIDTH, HEIGHT))
 TRACK_BORDER = scale(load('imgs/track-border.png'), (WIDTH, HEIGHT))
 FINISH_FLAG = scale(load('imgs/finish.png'), (FF_TARGET_W, FF_TARGET_H))
-GREEN_CAR = scale(load('imgs/green-car.png'), (scale_x(38), scale_y(76)))
-GREY_CAR = scale(load('imgs/grey-car.png'), (scale_x(38), scale_y(76)))
-PURPLE_CAR = scale(load('imgs/purple-car.png'), (scale_x(38), scale_y(76)))
-RED_CAR = scale(load('imgs/red-car.png'), (scale_x(38), scale_y(76)))
-WHITE_CAR = scale(load('imgs/white-car.png'), (scale_x(38), scale_y(76)))
+GREEN_CAR = scale(load('imgs/green-car.png'), (scale_x(38, target=WIDTH*COEF), scale_y(76, target=HEIGHT*COEF)))
+GREY_CAR = scale(load('imgs/grey-car.png'), (scale_x(38, target=WIDTH*COEF), scale_y(76, target=HEIGHT*COEF)))
+PURPLE_CAR = scale(load('imgs/purple-car.png'), (scale_x(38, target=WIDTH*COEF), scale_y(76, target=HEIGHT*COEF)))
+RED_CAR = scale(load('imgs/red-car.png'), (scale_x(38, target=WIDTH*COEF), scale_y(76, target=HEIGHT*COEF)))
+WHITE_CAR = scale(load('imgs/white-car.png'), (scale_x(38, target=WIDTH*COEF), scale_y(76, target=HEIGHT*COEF)))
