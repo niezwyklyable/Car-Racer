e00162f2dac7f97c69ce0d131b397b8f385e98c4
niezwyklyable <niezwyklyable@gmail.com>
2021-11-18 18:01:45 +0100

    Decreased game difficulty
     - decreased MAX_SPEED and ACCEL and increased DISTANCE_THRESHOLD for each level in the AICar class by overriding set_parameters method
     - trained new Q tables for each level due to AI car speed change (had to increased MAX_FRAMES from 2000 to 2500 due to index exceedance for level 1)
    
    	modified:   cc/AIcar.py
    	modified:   cc/constants.py
    	modified:   q_table-level-1.pickle
    	modified:   q_table-level-2.pickle
    	modified:   q_table-level-3.pickle

--- cc/AIcar.py
+++ cc/AIcar.py
@@ -16,3 +16,19 @@ class AICar(Car):
             self.turn_right()
         else:
             pass # do nothing (only move_forward() method is called)
+
+    def set_parameters(self):
+        # ACCEL - acceleration of a car (realistic feature)
+        # DISTANCE_THRESHOLD - the distance that the car has to rode to be able to collide with the finish flag (measured on the manual tests)
+        if self.lvl == 1:
+            self.MAX_SPEED = 2
+            self.ACCEL = 0.03
+            self.DISTANCE_THRESHOLD = 1300
+        elif self.lvl == 2:
+            self.MAX_SPEED = 3
+            self.ACCEL = 0.05
+            self.DISTANCE_THRESHOLD = 1000
+        else:
+            self.MAX_SPEED = 4
+            self.ACCEL = 0.07
+            self.DISTANCE_THRESHOLD = 800
